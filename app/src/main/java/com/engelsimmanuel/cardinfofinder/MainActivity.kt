cd7f6f4e75e932fabd4e93ece05345819e665090
engels <engelmmanuel@gmail.com>
2021-02-03 23:40:01 +0100

    Added dimens.xml, changed app name in strings.xml, added internet and camera permissions, added code to check for dangerous 'camera' permission

--- app/src/main/java/com/engelsimmanuel/cardinfofinder/MainActivity.kt
+++ app/src/main/java/com/engelsimmanuel/cardinfofinder/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.engelsimmanuel.cardinfofinder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CAMERA_REQUEST_CODE: Int = 1997
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        when (ActivityCompat.checkSelfPermission(
+            this@MainActivity,
+            Manifest.permission.CAMERA
+        ) != PackageManager.PERMISSION_GRANTED) {
+            true -> {
+                // use camera hardware here
+            }
+            false -> {
+                // request user permisssion to access camera
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_REQUEST_CODE
+                )
+            }
+        }
+
     }
 }
